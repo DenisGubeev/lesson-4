560d5cb2b29b12e66da5501d420ca19dfb49c70b
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-29 15:20:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,24 +12,24 @@ $api = file_get_contents("http://api.openweathermap.org/data/2.5/weather?id=" . 
 $city_list_file = file_get_contents("city.list.json");
 $decode_api = json_decode($api, true);
 $decode_city = json_decode($city_list_file, true);
-// Город
+
 $city_name = $decode_api['name'];
-// Погода
+
 $weather_desc = $decode_api['weather'][0]['description'];
 $pressure = $decode_api['main']['pressure'];
 $humidity = $decode_api['main']['humidity'];
 $coord_lon = $decode_api['coord']['lon'];
 $coord_lat = $decode_api['coord']['lat'];
-// Градусы
+
 $temp = $decode_api['main']['temp'];
-// Пересчитываем градусы
+
 $temp_celsius = $temp - 273;
 $temp_celsius = round($temp_celsius, 1). ' C&deg';
-// Добавляем плюсик
+
 if ($temp_celsius > 0) {
     $temp_celsius = str_pad($temp_celsius, strlen($temp_celsius)+1, "+", STR_PAD_LEFT);
 }
-// Иконка погоды
+
 $icon = $decode_api['weather'][0]['icon'];
 $icon_url = 'http://openweathermap.org/img/w/' . $icon . '.png';
 ?>
